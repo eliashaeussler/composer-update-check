2b4e93fc6b4668b862f168c52816f40acd42d72c
Elias Häußler <elias@haeussler.dev>
2023-12-08 21:40:01 +0100

    [BUGFIX] Add missing SeverityLevel enum

---
+++ src/Entity/Security/SeverityLevel.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Composer package "eliashaeussler/composer-update-check".
+ *
+ * Copyright (C) 2023 Elias Häußler <elias@haeussler.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace EliasHaeussler\ComposerUpdateCheck\Entity\Security;
+
+/**
+ * SeverityLevel.
+ *
+ * @author Elias Häußler <elias@haeussler.dev>
+ * @license GPL-3.0-or-later
+ *
+ * @see https://nvd.nist.gov/vuln-metrics/cvss
+ */
+enum SeverityLevel: string
+{
+    case Low = 'low';
+    case Medium = 'medium';
+    case High = 'high';
+    case Critical = 'critical';
+
+    public function compareTo(self $other): int
+    {
+        return $this->getInternalLevel() <=> $other->getInternalLevel();
+    }
+
+    private function getInternalLevel(): int
+    {
+        return match ($this) {
+            self::Low => 1,
+            self::Medium => 2,
+            self::High => 3,
+            self::Critical => 4,
+        };
+    }
+}
